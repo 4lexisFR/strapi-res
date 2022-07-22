1d3a183d264e8d1fed47dd0c911aa2c30cac5124
Alexis <4lexis.fr@gmail.com>
2022-07-23 01:53:28 +0200

    fffff

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     proxy: true,
     url: env('https://res-backend-server.herokuapp.com/'),
     app: { 
-      keys: "DmGwafWZ1MGxrAt7vqHOGg==,lRg5b70+AZaZdHq7qTrqYg==,OB9PblemhTv1uqeHQO5eig==,NZ8fScsGI5Jf0VTgEV0E2g=="
+        keys: env.array("APP_KEYS", ["DATABASE_URL", "MY_HEROKU_URL", "NODE_ENV"]),
     },
   })
